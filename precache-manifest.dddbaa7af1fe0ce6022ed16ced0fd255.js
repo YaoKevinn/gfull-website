eb9aff3d2645d10aad4ea2c5e565c5e1ce58bb6a
YaoKevinn <vasty85817@gmail.com>
2020-08-25 21:40:59 -0300

    Updates

---
+++ precache-manifest.dddbaa7af1fe0ce6022ed16ced0fd255.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d895205c7dcaa414a29d1b1beef628",
+    "url": "/gfull-website/index.html"
+  },
+  {
+    "revision": "6ca88d6629c75995e621",
+    "url": "/gfull-website/static/css/main.4b9423fc.chunk.css"
+  },
+  {
+    "revision": "df0015751c522c75b777",
+    "url": "/gfull-website/static/js/2.7f4d31b2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gfull-website/static/js/2.7f4d31b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca88d6629c75995e621",
+    "url": "/gfull-website/static/js/main.a7b90609.chunk.js"
+  },
+  {
+    "revision": "5eb7584c7ac98b239e11",
+    "url": "/gfull-website/static/js/runtime-main.7f68f24a.js"
+  },
+  {
+    "revision": "784f8a2e9dbbc246562c26f285562a5d",
+    "url": "/gfull-website/static/media/aduki.784f8a2e.jpg"
+  },
+  {
+    "revision": "d46b2a5e73f8265e6ceb7eaff30d3f1f",
+    "url": "/gfull-website/static/media/almendraNonPareil.d46b2a5e.jpg"
+  },
+  {
+    "revision": "ea07a9b88155c17f0741d926b7b7ff59",
+    "url": "/gfull-website/static/media/arandanosRojos.ea07a9b8.jpg"
+  },
+  {
+    "revision": "683be9065976359f97f994a6f8e14f5b",
+    "url": "/gfull-website/static/media/bg.683be906.png"
+  },
+  {
+    "revision": "b3d6472e068489d684cd153aa2e949b5",
+    "url": "/gfull-website/static/media/castania.b3d6472e.jpg"
+  },
+  {
+    "revision": "60183f8d65c2daa04c5f8e4ae2ee23ae",
+    "url": "/gfull-website/static/media/ciruela.60183f8d.jpg"
+  },
+  {
+    "revision": "b2085bd460c9562df6370365eb752937",
+    "url": "/gfull-website/static/media/contentBox.b2085bd4.png"
+  },
+  {
+    "revision": "cfc6507c018491be2747c1c89e1b6061",
+    "url": "/gfull-website/static/media/logo.cfc6507c.png"
+  },
+  {
+    "revision": "ec1423f75bb52db1141cf10de0ad70ab",
+    "url": "/gfull-website/static/media/mani.ec1423f7.jpg"
+  },
+  {
+    "revision": "37cc22f244c65fbb5f890abafc7466a9",
+    "url": "/gfull-website/static/media/maniSinSal.37cc22f2.jpg"
+  },
+  {
+    "revision": "8d39f63f6eb874330938005d4a97d5bd",
+    "url": "/gfull-website/static/media/nuezMariposa.8d39f63f.jpg"
+  },
+  {
+    "revision": "96e348fbb4b6b2ba62dc6ebca38d5d4b",
+    "url": "/gfull-website/static/media/pistacho.96e348fb.jpg"
+  },
+  {
+    "revision": "a2233dea3d78505ef5ba75d81adb9fb6",
+    "url": "/gfull-website/static/media/porotoMung.a2233dea.jpg"
+  }
+]);
